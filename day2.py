1dc701508ecba85ae33d1612c9de7e9e9e55df0c
lrhyde <lrhyde13@gmail.com>
2024-12-02 15:25:03 -0600

    day 2 part 2

--- day2.py
+++ day2.py
@@ -1,12 +1,27 @@
 infile = open("day2.txt")
 safe = 0
-for line in infile:
-    nums = [int(n) for n in line.strip().split()]
-    diffs = [nums[i+1]-nums[i] for i in range(len(nums)-1)]
+
+def levelsafe(diffs):
     if(min(diffs)>0):
         if(max(diffs)<=3):
-            safe+=1
+            return True
     if(max(diffs)<0):
         if(min(diffs)>=-3):
-            safe+=1
+            return True
+    return False
+
+def calcdiffs(nums):
+    diffs = [nums[i+1]-nums[i] for i in range(len(nums)-1)]
+    return diffs
+
+for line in infile:
+    nums = [int(n) for n in line.strip().split()]
+    diffs = calcdiffs(nums)
+    lsafe = levelsafe(diffs)
+    for i in range(len(nums)):
+        if(not lsafe):
+            n2 = [nums[j] for j in range(len(nums)) if(j!=i)]
+            diffs = calcdiffs(n2)
+            lsafe = levelsafe(diffs)
+    safe+=lsafe
 print(safe)
