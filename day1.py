17207dabe0bde7b703f90e56dd82f90fe26ea5c2
lrhyde <lrhyde13@gmail.com>
2024-12-01 19:06:07 -0600

    day 1 part 2

--- day1.py
+++ day1.py
@@ -9,3 +9,11 @@ list1 = sorted(list1)
 list2 = sorted(list2)
 diffs = [abs(list1[i] - list2[i]) for i in range(len(list1))]
 print(sum(diffs))
+
+# part 2:
+# This time, you'll need to figure out exactly how often each number from the left list appears in the right list. 
+# Calculate a total similarity score by adding up each number in the left list after multiplying it by the number 
+# of times that number appears in the right list.
+
+similarities = [item * list2.count(item) for item in list1]
+print(sum(similarities))
